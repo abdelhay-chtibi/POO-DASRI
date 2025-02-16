a417ff3c4f9345b8c5ed6e15cb6dddbd7aed61db
abdelhay-chtibi <142673856+abdelhay-chtibi@users.noreply.github.com>
2025-02-16 14:38:39 +0100

    seance 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x;
+        x = 5;
+        System.out.println(x);
+        char a = 'a';
+        // char b = "r";
+        String t = "chaine";
+        // byte bb = 123456789;
+        System.out.println(t);
+    }
+}
---
+++ src/main/java/org/example/Operation.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Operation {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 5;
+        int sum;
+        sum = x + y;
+        System.out.println("Resultat de la somme est: "+sum);
+        int sous;
+        sous = x - y;
+        System.out.println("Resultat de la soustratction: "+ sous);
+        double div;
+        div = (double) x / y;
+        System.out.println("Resultat de la division: "+ div);
+    }
+}
