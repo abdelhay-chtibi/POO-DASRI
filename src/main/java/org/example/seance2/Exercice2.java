2dcd62281c970abdb67495e55b672f8e06e0a2a5
abdelhay-chtibi <142673856+abdelhay-chtibi@users.noreply.github.com>
2025-02-23 16:02:23 +0100

    commit

---
+++ src/main/java/org/example/seance2/Exercice2.java
@@ -0,0 +1,20 @@
+package org.example.seance2;
+
+public class Exercice2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 60) {
+            if (i % 2 == 0) {
+                System.out.println("paire");
+            } else if (i % 3 == 0) {
+                System.out.println("M3");
+            } else if (i % 6 == 0) {
+                System.out.println("Fizz");
+            }else {
+                System.out.println(i);
+            }
+            i++;
+        }
+
+    }
+}
