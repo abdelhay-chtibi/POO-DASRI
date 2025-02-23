3b0118c2433526250f1c0488b5506bda7aa72ae7
abdelhay-chtibi <142673856+abdelhay-chtibi@users.noreply.github.com>
2025-02-23 12:22:14 +0100

    comparaissant

---
+++ src/main/java/org/example/seance2/Comparaissant.java
@@ -0,0 +1,22 @@
+package org.example.seance2;
+
+public class Comparaissant {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = 7 < 6; // false
+        boolean z =! y; // true
+
+        System.out.println(y);
+        System.out.println(z);
+
+        System.out.println(x =! false); // true
+        System.out.println(x && y); //false
+        System.out.println(x || y); // true
+        System.out.println(!y); // true
+        System.out.println(x && (y || z)); // true
+        System.out.println((x && y) || z); // false || true == true
+        System.out.println((!x || ! y) && (!z)); // true && false == false
+        System.out.println(!((x && y) || z)); // !(false || true) == ! true == false
+
+    }
+}
