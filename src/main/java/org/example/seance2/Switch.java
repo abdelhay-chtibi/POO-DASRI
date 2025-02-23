3b4b7ef3e1e307935d0672652902663c8bdf9311
abdelhay-chtibi <142673856+abdelhay-chtibi@users.noreply.github.com>
2025-02-23 14:11:32 +0100

    commit

--- src/main/java/org/example/seance2/Switch.java
+++ src/main/java/org/example/seance2/Switch.java
@@ -5,7 +5,32 @@ import java.util.Scanner;
 public class Switch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez un jour de la semaine :");
         String jour = scanner.nextLine();
-
+        switch (jour) {
+            case "Lundi":
+                System.out.printf("1%n");
+                break;
+            case "Mardi":
+                System.out.printf("2%n");
+                break;
+            case "Mercredi":
+                System.out.printf("3%n");
+                break;
+            case "Jeudi":
+                System.out.printf("4%n");
+                break;
+            case "Vendredi":
+                System.out.printf("5%n");
+                break;
+            case "Samedi":
+                System.out.printf("6%n");
+                break;
+            case "Dimanche":
+                System.out.printf("7%n");
+                break;
+            default:
+                System.out.printf("Jour invalide%n");
+        }
     }
 }
