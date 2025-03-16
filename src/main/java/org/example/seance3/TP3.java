0d623aa68370d3d3e08f2a0c18b43b1f2be5a5f3
abdelhay-chtibi <142673856+abdelhay-chtibi@users.noreply.github.com>
2025-03-16 18:44:13 +0000

    Method2

--- src/main/java/org/example/seance3/TP3.java
+++ src/main/java/org/example/seance3/TP3.java
@@ -1,2 +1,22 @@
-package org.example.seance3;public class TP3 {
+package org.example.seance3;
+
+import java.util.Scanner;
+
+public class TP3 {
+    public int Multiplication(int a, int b){
+    return a * b;
+}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez le premier: ");
+        int premierNombre = scanner.nextInt();
+
+        System.out.print("Entrez le nombre: ");
+        int deuxiemeNombre = scanner.nextInt();
+
+        TP3 Object = new TP3();
+        int resultat = Object.Multiplication(premierNombre, deuxiemeNombre);
+        System.out.println("La resultat de la multiplication de " + premierNombre + " et " + deuxiemeNombre + " est: " + resultat);
+        scanner.close();
+    }
 }
